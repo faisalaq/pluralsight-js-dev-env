8c3dc74134992ebadd07db3b90db95ca09f5a34a
faisalaq <faq0818@gmail.com>
2017-08-04 16:31:23 -0700

    added lint module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import numeral from 'numeral';
 
 const courseVal = numeral(100).format('$0,0.00');
-debugger;
+
+/*eslint-disable no-console*/
 console.log(`take this course for a cheap price of ${courseVal}`);
