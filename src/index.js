be40550abfac09efd9fff351e9445003721eeb07
Faisal <faq01818@gmail.com>
2017-08-07 09:04:16 -0700

    adding HTTP calls till 'Serving Mock Data'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import numeral from 'numeral';
+// import numeral from 'numeral';
 
-const courseVal = numeral(100).format('$0,0.00');
+// const courseVal = numeral(100).format('$0,0.00');
 
-/*eslint-disable no-console*/
-console.log(`take this course for a cheap price of ${courseVal}`);
+// /*eslint-disable no-console*/
+// console.log(`take this course for a cheap price of ${courseVal}`);
+
+
+import {getUsers} from './api/userApi';
+
+getUsers().then(result => {
+    let usersBody = "";
+    result.forEach(user =>{
+    usersBody+=`<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+    </tr>`
+    });
+
+global.document.getElementById('users').innerHTML = usersBody;
+});
